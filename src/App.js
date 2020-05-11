2629be5a6389b11a21c58c39cba63c71a1205e56
uploadercde360 <64452359+uploadercde360@users.noreply.github.com>
2020-05-12 00:37:54 +0800

    update version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Redirect } from "react-router-dom";
 
 import history from './history.js';
 import './App.css';
@@ -13,7 +13,8 @@ function App() {
   return (
     <Router history={history}>
       <div>
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
         <Route path="/main" component={Main} />
         <Route path="/plan" component={Plans} />
         <Route path="/setting" component={Settings} />
--- src/pages/Settings/AccountDetail/accountdetail.js
+++ src/pages/Settings/AccountDetail/accountdetail.js
@@ -13,20 +13,20 @@ class AccountDetail extends Component {
     return (
         <div className="main-block account-detail">
             <h5> Account Details  </h5>
-            <h6> Most Recent Order </h6>
+            <h6 className="mt-4"> Most Recent Order </h6>
             <Row className="mt-3">
                 <Col> 
-                    <span> Type: </span> <span> {account_details.type} </span>
+                    <span className="detail-title"> Type : </span> <span className="detail-content"> {account_details.type} </span>
                 </Col>
             </Row>
             <Row className="mt-2">
                 <Col> 
-                    <span> Quality: </span> <span> {account_details.quality} </span>
+                    <span className="detail-title"> Quality : </span> <span className="detail-content"> {account_details.quality} </span>
                 </Col>
             </Row>
             <Row className="mt-2">
                 <Col> 
-                    <span> Length: </span> <span> {account_details.length} </span>
+                    <span className="detail-title"> Length : </span> <span className="detail-content"> {account_details.length} </span>
                 </Col>
             </Row>
         </div>
